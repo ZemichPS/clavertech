4fa902b4df3ee6dfb6519ef5ed70468248fcda41
Eugene <compserv@yahoo.com>
2024-11-06 20:41:48 +0300

    add nex level security check

--- newsms/src/main/java/by/zemich/newsms/config/properties/CacheProperties.java
+++ newsms/src/main/java/by/zemich/newsms/config/properties/CacheProperties.java
@@ -6,6 +6,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties(prefix = "cache")
 @Data
 public class CacheProperties {
+    private boolean enabled;
     private int size;
     private String algorithm;
 }
--- userms/src/main/java/by/zemich/userms/security/configuration/SecurityConfig.java
+++ userms/src/main/java/by/zemich/userms/security/configuration/SecurityConfig.java
@@ -1,7 +1,9 @@
 package by.zemich.userms.security.configuration;
 
+import by.zemich.userms.dao.repository.UserRepository;
 import by.zemich.userms.security.filter.JWTFilter;
 import by.zemich.userms.security.properties.JWTProperty;
+import by.zemich.userms.security.service.UserAdminOrAddressOwnerAuthorizationManager;
 import by.zemich.userms.security.utils.JWTHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,12 +13,15 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.authorization.AuthorizationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
+import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
@@ -27,13 +32,18 @@ import static org.springframework.security.web.util.matcher.AntPathRequestMatche
 @Profile("prod")
 public class SecurityConfig {
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http, JWTFilter jwtFilter) throws Exception {
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity http,
+            JWTFilter jwtFilter,
+            AuthorizationManager<RequestAuthorizationContext> userAdminOrAddressOwnerAuthorizationManager
+    ) throws Exception {
         return http
                 .authorizeHttpRequests((authorize) -> authorize
                         .requestMatchers(antMatcher(HttpMethod.GET, "/api/v1/users"),
-                                antMatcher(HttpMethod.GET, "/api/v1/users/**"),
                                 antMatcher(HttpMethod.PATCH, "/api/v1/users/**/deactivation"),
                                 antMatcher(HttpMethod.PATCH, "/api/v1/users/**/activation")).hasRole("ADMIN")
+                        .requestMatchers(antMatcher(HttpMethod.GET, "/api/v1/users/{userId}"))
+                        .access(userAdminOrAddressOwnerAuthorizationManager)
                         .requestMatchers(antMatcher(HttpMethod.POST)).permitAll()
                         .requestMatchers(antMatcher(HttpMethod.GET, "/api/v1/auth"), antMatcher(HttpMethod.PATCH, "/api/v1/auth")).permitAll())
                 .csrf().disable()
@@ -66,8 +76,12 @@ public class SecurityConfig {
     }
 
     @Bean
-    JWTHandler jwtHandler(JWTProperty jwtProperty) {
+    public JWTHandler jwtHandler(JWTProperty jwtProperty) {
         return new JWTHandler(jwtProperty);
     }
 
+    @Bean
+    public AuthorizationManager<RequestAuthorizationContext> userAdminOrAddressOwnerAuthorizationManager(UserRepository userRepository) {
+        return new UserAdminOrAddressOwnerAuthorizationManager(userRepository);
+    }
 }
